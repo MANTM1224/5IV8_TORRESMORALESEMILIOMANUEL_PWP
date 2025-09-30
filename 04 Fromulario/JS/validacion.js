ef5e7c2576076982ebc8082037a528a470f39796
MANTM1224 <etorresm2304@alumno.ipn.mx>
2025-09-30 11:38:31 -0600

    intentando arreglar errores otra vez

--- 04 Fromulario/JS/validacion.js
+++ 04 Fromulario/JS/validacion.js
@@ -10,78 +10,80 @@ const -> variable local, no se puede reasignar ni redeclarar
 
 */
 
-function validar (formulario) {
-//Validar que el campo no acepte menos de 3 caracteres
-if (formulario.nombre.value.length < 3 ) {
-    alert("Por favor escribe más de 3 caracteres en el campo nombre");
-    formulario.nombre.focus();
-    return false;
-}
-// Validación letras
-var checkStr = formulario.nombre.value;
-var abcOK = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ" + "abcdefghijklmnñopqrstuvwxyz";
-var allvalido = true;
 
-// Comparar cadena de nombre con el resultado de abc
-for (var i = 0; i < checkStr.length; i++) {
-    var caracteres = checkStr.charAt(i);
-    var letraValida = false;
-    for (var j = 0; j < abcOK.length; j++) {
-        if (caracteres == abcOK.charAt(j)) {
-            letraValida = true;
+function validar(formulario) {
+    // Validar que el campo no acepte menos de 3 caracteres
+    if (formulario.nombre.value.length < 3) {
+        alert("Por favor escribe más de 3 caracteres en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+    
+    // Validación letras (agregué espacios para nombres compuestos)
+    var checkStr = formulario.nombre.value;
+    var abcOK = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ" + "abcdefghijklmnñopqrstuvwxyz ";
+    var allvalido = true;
+
+    // Comparar cadena de nombre con el resultado de abc
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        var letraValida = false;
+        for (var j = 0; j < abcOK.length; j++) {
+            if (caracteres == abcOK.charAt(j)) {
+                letraValida = true;
+                break;
+            }
+        }
+        if (!letraValida) {
+            allvalido = false;
             break;
         }
     }
-    if (!letraValida) {
-        allvalido = false;
-        break;
+    if (allvalido == false) {
+        alert("Ingresar un nombre válido");
+        formulario.nombre.focus();
+        return false;
     }
-}
-if (allvalido == false) {
-    alert("Ingresar un nombre válido");
-    formulario.nombre.focus();
-    return false;
-}
 
-// Validación solo números
-var checkStr = formulario.edad.value;
-var numOK = "1234567890";
-var allvalido = true;
+    // Validación solo números
+    var checkStr = formulario.edad.value;
+    var numOK = "1234567890";
+    var allvalido = true;
 
-// Comparar cadena de número con el numOK
-for (var i = 0; i < checkStr.length; i++) {
-    var caracteres = checkStr.charAt(i);
-    var numeroValido = false;
-    for (var j = 0; j < numOK.length; j++) {
-        if (caracteres == numOK.charAt(j)) {
-            numeroValido = true;
+    // Comparar cadena de número con el numOK
+    for (var i = 0; i < checkStr.length; i++) {
+        var caracteres = checkStr.charAt(i);
+        var numeroValido = false;
+        for (var j = 0; j < numOK.length; j++) {
+            if (caracteres == numOK.charAt(j)) {
+                numeroValido = true;
+                break;
+            }
+        }
+        if (!numeroValido) {
+            allvalido = false;
             break;
         }
     }
-    if (!numeroValido) {
-        allvalido = false;
-        break;
+    if (!allvalido) {
+        alert("Ingrese únicamente números");
+        formulario.edad.focus();
+        return false;
     }
-}
-if (!allvalido) {
-    alert("Ingrese únicamente números");
-    formulario.edad.focus();
+    
+    // Validación correo electrónico
+    var correo = formulario.correo.value;
+    var regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexCorreo.test(correo)) {
+        alert("Ingrese un correo electrónico válido");
+        formulario.correo.focus();
+        return false;
+    }
+
+    // Si todas las validaciones pasan
+    alert("¡Formulario válido! Datos procesados correctamente:\n" +
+          "Nombre: " + formulario.nombre.value + "\n" +
+          "Edad: " + formulario.edad.value + "\n" +
+          "Email: " + formulario.correo.value);
     return false;
-}
-//Validación correo electrónico
-var correo = formulario.correo.value;
-var regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-if (!regexCorreo.test(correo)) {
-   alert("Ingrese un correo electrónico válido");
-   formulario.correo.focus();
-   return false;
-//}
-
-} 
-
-
-//Validar correo electrónico que acepte formato texto@texto.texto
-//texto.texto@texto.texto 
-//texto.texto@texto.texto 
-
 }
