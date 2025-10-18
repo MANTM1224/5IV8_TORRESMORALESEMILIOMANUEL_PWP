b310e6fba24c528550d2a99a43419317555b3a71
MANTM1224 <etorresm2304@alumno.ipn.mx>
2025-10-18 17:31:57 -0600

    Se hizo el problema 2

--- 07 EJERCICIOS 02/js/script.js
+++ 07 EJERCICIOS 02/js/script.js
@@ -26,7 +26,17 @@ function problema1() {
 }
 
 function problema2() {
-  
+  var num1 = parseFloat(document.getElementById("p2-num1").value);
+  var num2 = parseFloat(document.getElementById("p2-num2").value);
+  var num3 = parseFloat(document.getElementById("p2-num3").value);
+
+  if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    document.getElementById("p2-resultado").innerText = "Por favor, ingrese números válidos.";
+    return;
+  }
+  var mayor = Math.max(num1, num2, num3);
+  document.getElementById('p2-output').textContent = 
+        `Números ingresados: ${num1}, ${num2}, ${num3}\nEl número mayor es: ${mayor}`;
 }
 
 function problema3() {
