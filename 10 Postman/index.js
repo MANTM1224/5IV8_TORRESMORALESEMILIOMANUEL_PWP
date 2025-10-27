4cb86c62ba2f7da6d29596be8788d9ced6bea55c
MANTM1224 <etorresm2304@alumno.ipn.mx>
2025-10-26 23:01:54 -0600

    Arreglando mi desmadre jej v1.1

---
+++ 10 Postman/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mirrow = require('./endpoints/mirrow');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+//definios cada una de las rutas
+app.get('/', mirrow);
+app.post('/', mirrow);
+app.put('/', mirrow);
+app.patch('/', mirrow);
+app.delete('/', mirrow);
+app.head('/', mirrow);
+
+app.listen(port, () => {
+    console.log("Servidor escuchando");
+});
